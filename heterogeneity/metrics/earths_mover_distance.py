660271b0c9ad3b90d36e6a3580e5e8b6f84ddab1
Adam Narozniak <narozniak.adam4@gmail.com>
2024-03-26 10:07:53 +0100

    Add earths mover distance

---
+++ heterogeneity/metrics/earths_mover_distance.py
@@ -0,0 +1,41 @@
+from heterogeneity.metrics.utils import compute_counts, compute_distributions
+from flwr_datasets.partitioner import Partitioner
+import numpy as np
+from scipy.stats import wasserstein_distance
+
+
+def compute_earths_mover_distance(partitioner: Partitioner, label_name: str=None, aggregation_type: str = None):
+    """
+
+    Parameters
+    ----------
+    partitioner
+    method: str
+        "cpm-with-global", "pair-wise"
+
+    Returns
+    -------
+
+    """
+    dataset = partitioner.dataset
+    all_labels = dataset.features["label"].str2int(dataset.features["label"].names)
+
+    partitions = []
+    for i in range(partitioner.num_partitions):
+        partitions.append(partitioner.load_partition(i))
+
+    # # Calculate global distribution
+    # global_distribution = compute_distributions(dataset['label'], all_labels)
+    #
+    # # Calculate (local) distribution for each client
+    # local_distributions = []
+    # for partition in partitions:
+    #     distribution = compute_distributions(partition['label'], all_labels)
+    #     local_distributions.append(distribution)
+
+    partitions_earths_mover_distance = []
+    for partition in partitions:
+        emd = wasserstein_distance(dataset["label"], partition["label"])
+        partitions_earths_mover_distance.append(emd)
+
+    return partitions_earths_mover_distance, np.average(partitions_earths_mover_distance, weights=list(map(len, partitions)))
