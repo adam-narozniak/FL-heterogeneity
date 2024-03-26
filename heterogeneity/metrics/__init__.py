5b2cd92d6a657ba4a6a43de238eab9d3eb2524ca
Adam Narozniak <narozniak.adam4@gmail.com>
2024-03-26 10:09:51 +0100

    Expose metrics from metrics package

---
+++ heterogeneity/metrics/__init__.py
@@ -0,0 +1,5 @@
+from heterogeneity.metrics.hellinger_distance import compute_hellinger_distance
+from heterogeneity.metrics.kl_divergence import compute_kl_divergence
+from heterogeneity.metrics.earths_mover_distance import compute_earths_mover_distance
+
+__all__ = ["compute_hellinger_distance", "compute_kl_divergence", "compute_earths_mover_distance"]
