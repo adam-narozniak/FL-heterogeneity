34881c64dc5fea3957a44748ce1c66041bf5db00
Adam Narozniak <narozniak.adam4@gmail.com>
2024-05-13 17:12:52 +0200

    Move back to full configs

--- configs/partitioner_configs.py
+++ configs/partitioner_configs.py
@@ -4,15 +4,15 @@ from flwr_datasets.partitioner import IidPartitioner, DirichletPartitioner, \
 config_iid_partitioner = {
     "object": IidPartitioner,
     "param_grid": {
-        "num_partitions": [3, 10]  # , 30, 100, 300, 1000]
+        "num_partitions": [3, 10, 30, 100, 300, 1000]
     }
 }
 
 config_dirichlet_partitioner = {
     "object": DirichletPartitioner,
     "param_grid": {
-        "num_partitions": [3, 10],  # , 30, 100, 300, 1000],
-        "alpha": [0.1, 0.3],#, 1., 3., 10., 100., 1000.],
+        "num_partitions": [3, 10, 30, 100, 300, 1000],
+        "alpha": [0.1, 0.3, 1., 3., 10., 100., 1000.],
         "partition_by": ["label"]
     }
 }
@@ -22,26 +22,24 @@ config_natural_id_partitioner = {
     "param_grid": {}
 }
 
-# config_shard_partitioner = {
-#     "object": ShardPartitioner,
-#     "param_grid": {
-#         "num_partitions": [3, 10, 30, 100, 300, 1000],
-#         "num_shards_per_partition": [2, 3, 4, 5],
-#         "partition_by": ["label"]
-#     }
-# }
+config_shard_partitioner = {
+    "object": ShardPartitioner,
+    "param_grid": {
+        "num_partitions": [3, 10, 30, 100, 300, 1000],
+        "num_shards_per_partition": [2, 3, 4, 5],
+        "partition_by": ["label"]
+    }
+}
 
-# config_inner_dirichlet_partitioner = {
-#     "object": InnerDirichletPartitioner,
-#     "param_grid": {
-#         "alpha": [0.1, 0.3, 1., 3., 10., 100., 1000.],
-#         "sigma": [0.1, 0.3, 1., 3.],
-#         "partition_by": ["label"]
-#     }
-# }
+config_inner_dirichlet_partitioner = {
+    "object": InnerDirichletPartitioner,
+    "param_grid": {
+        "alpha": [0.1, 0.3, 1., 3., 10., 100., 1000.],
+        "sigma": [0.1, 0.3, 1., 3.],
+        "partition_by": ["label"]
+    }
+}
 
 natural_partitioner_configs = [config_natural_id_partitioner]
 
-no_natural_partitioner_configs = [config_iid_partitioner, config_dirichlet_partitioner,]
-                       # config_shard_partitioner]  # ,
-# config_inner_dirichlet_partitioner]
+no_natural_partitioner_configs = [config_iid_partitioner, config_dirichlet_partitioner, config_shard_partitioner, config_inner_dirichlet_partitioner]
