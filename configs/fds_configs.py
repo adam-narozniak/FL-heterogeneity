fd3f0f1205128acb60a5697337e421cdf77265e8
Adam Narozniak <narozniak.adam4@gmail.com>
2024-05-14 16:06:21 +0200

    Update FDS config

--- configs/fds_configs.py
+++ configs/fds_configs.py
@@ -8,21 +8,12 @@ natural_datasets_param_grid = [
         "label_name": ["label"]
 
     },
-    # {
-    #     "dataset": ["adamnarozniak/femnist"],
-    #     "split": ["train"],
-    #     "shuffle": [False],
-    #     "partition_by": ["writer_id"],
-    #     "label_name": ["character"]
-    #
-    # },
-
     {
-        "dataset": ["sentiment140"],
+        "dataset": ["adamnarozniak/femnist"],
         "split": ["train"],
         "shuffle": [False],
-        "partition_by": ["user"],
-        "label_name": ["sentiment"]
+        "partition_by": ["writer_id"],
+        "label_name": ["character"],
 
     },
     {
@@ -33,12 +24,21 @@ natural_datasets_param_grid = [
         "label_name": ["y"]
 
     },
+    {
+        "dataset": ["sentiment140"],
+        "split": ["train"],
+        "shuffle": [False],
+        "partition_by": ["user"],
+        "label_name": ["sentiment"]
+
+    },
 ]
 
 no_natural_datasets_param_grid = [
     {
-        "dataset": ["cifar10", "mnist"],
+        "dataset": [ "cifar10", "mnist"],
         "label_name": ["label"],
+        "partition_by": ["label"],
         "split": ["train"],
         "shuffle": [True],
         "seed": list(range(42, 47))
@@ -46,6 +46,7 @@ no_natural_datasets_param_grid = [
     {
         "dataset": ["cifar100"],
         "label_name": ["fine_label"],
+        "partition_by": ["fine_label"],
         "split": ["train"],
         "shuffle": [True],
         "seed": list(range(42, 47))
@@ -53,7 +54,10 @@ no_natural_datasets_param_grid = [
 ]
 # Add the natural_id datasets to the simulate the division on them
 for natural_dataset_param_grid in natural_datasets_param_grid:
+    fds_copy = natural_dataset_param_grid.copy()
+    fds_copy["partition_by"] = fds_copy["label_name"]
+    fds_copy["shuffle"] = [True]
     no_natural_datasets_param_grid.append(
-        {**natural_dataset_param_grid, "seed": list(range(42, 47))})
+        {**fds_copy, "seed": list(range(42, 47))})
 
 fds_param_grids = [*no_natural_datasets_param_grid, *natural_datasets_param_grid]
