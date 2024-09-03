e0b67b0d13adf7e424f7044e52d859bd256860d2
Adam Narozniak <narozniak.adam4@gmail.com>
2024-09-03 21:20:01 +0200

    Update experiments file to include FL experiments

--- heterogeneity/experiments.py
+++ heterogeneity/experiments.py
@@ -1,18 +1,21 @@
+from cgi import test
 import itertools
 from pathlib import Path
 
+from matplotlib import transforms
 import numpy as np
 import pandas as pd
 from flwr_datasets import FederatedDataset
 
-from configs.fds_configs import no_natural_datasets_param_grid, natural_datasets_param_grid, config_femnist, config_mnist
+from configs.fl_configs import fl_configs
+from configs.fds_configs import no_natural_datasets_param_grid, natural_datasets_param_grid, config_femnist, config_mnist, config_cifar10
 from configs.metrics_configs import metrics_configs
 from configs.partitioner_configs import natural_partitioner_configs, no_natural_partitioner_configs, config_class_constrained, config_iid_partitioner, ClassConstrainedPartitioner
+from heterogeneity.fl.fl_loop_fnc import create_dataloaders, get_net, run_fl_experiment
 from heterogeneity.utils import create_lognormal_partition_sizes
 from flwr_datasets.partitioner import DirichletPartitioner, ShardPartitioner, NaturalIdPartitioner, InnerDirichletPartitioner
 
-
-def run_experiments_from_configs(datasets_param_grid, partitioner_param_grid, metrics_configs, run_fl):
+def run_experiments_from_configs(datasets_param_grid, partitioner_param_grid, metrics_configs, run_heterogeneity, run_fl, fl_configs):
     # iterate over all configs/some other specification
     # save the results of each of the runs
 
@@ -73,42 +76,111 @@ def run_experiments_from_configs(datasets_param_grid, partitioner_param_grid, me
                     label_name = None
                     if "label_name" in fds_kwargs:
                         label_name = fds_kwargs.pop("label_name")
+                        features_name = fds_kwargs.pop("features_name")
                     fds = FederatedDataset(**fds_kwargs)
                     
                     # Different metrics calculation
-                    for metric_config in metrics_configs:
-                        metrics_fnc = metric_config["object"]
-                        print("metric function:")
-                        print(metrics_fnc)
-                        if label_name is not None:
-                            metric_config["kwargs"]["label_name"] = label_name
-                        metrics_kwargs = {"partitioner": partitioner,
-                                          **metric_config["kwargs"]}
-                        print("metrics kwargs")
-                        print(metrics_kwargs)
-                        try:
-                            # trigger the assigment of the data
-                            _ = fds.load_partition(0)
-                            metric_list, metric_avg = metrics_fnc(**metrics_kwargs)
-                            if any([metric_list_val in [np.inf, -np.inf] for
-                                    metric_list_val in metric_list]):
+                    if run_heterogeneity:
+                        for metric_config in metrics_configs:
+                            metrics_fnc = metric_config["object"]
+                            print("metric function:")
+                            print(metrics_fnc)
+                            if label_name is not None:
+                                metric_config["kwargs"]["label_name"] = label_name
+                            metrics_kwargs = {"partitioner": partitioner,
+                                            **metric_config["kwargs"]}
+                            print("metrics kwargs")
+                            print(metrics_kwargs)
+                            try:
+                                # trigger the assigment of the data
+                                _ = fds.load_partition(0)
+                                metric_list, metric_avg = metrics_fnc(**metrics_kwargs)
+                                if any([metric_list_val in [np.inf, -np.inf] for
+                                        metric_list_val in metric_list]):
+                                    metric_list, metric_avg = np.nan, np.nan
+                            except ValueError as e:
+                                print(e)
+                                print(f"Sampling failed")
                                 metric_list, metric_avg = np.nan, np.nan
-                        except ValueError as e:
-                            print(e)
-                            print(f"Sampling failed")
-                            metric_list, metric_avg = np.nan, np.nan
 
-                        print("Metric avg: ", metric_avg)
-                        if metrics_fnc.__name__ not in metrics_avg_results:
-                            metrics_avg_results[metrics_fnc.__name__] = pd.DataFrame([], columns=[*list(single_partitioner_config.keys()), "fds_seed", "metric_name", "metric_value"])
-                            # metrics_avg_results[metrics_fnc.__name__]["fds_seed"] = single_fds.get("seed", "default")
+                            print("Metric avg: ", metric_avg)
+                            # Save exmperiments results as quick as they are available
+                            # (then append as the new experiments come)
+                            save_results_dir_path = (f"results-2024-03-09/{single_fds['dataset']}/"
+                                                                            f"{partitioner_signature.__name__}/{metrics_fnc.__name__}.csv")
+                            save_results_dir_path = Path(save_results_dir_path)
+                            include_header = True
+                            if save_results_dir_path.exists():
+                                include_header = False
+                            save_results_dir_path.parent.mkdir(parents=True, exist_ok=True)
+                            pd.DataFrame([[*single_partitioner_config.values(), single_fds.get("seed", "default"), metrics_fnc.__name__, metric_avg]], columns=[*list(single_partitioner_config.keys()), "fds_seed", "metric_name", "metric_value"]).to_csv(save_results_dir_path, index=False, mode="a", header=include_header)
+                    if run_fl:
+                        for fl_config in fl_configs:
+                            print(f"Running FL for {single_fds['dataset']} with {partitioner_signature.__name__}")
+                            try:
+                                trainloaders, testloaders, centralized_dataloader = create_dataloaders(fds, seed=42)
+                                dataset_name = fds.load_split("train").info.dataset_name
+                                num_classes = len(fds.load_split("train").unique(label_name))
+                                net = get_net(dataset_name, num_classes)
+                                n_comunication_rounds = fl_config["n_comunication_rounds"]
+                                num_partitions = fds.partitioners["train"].num_partitions
+                                n_clients_per_round_train = num_partitions if num_partitions <= 10 else int(fl_config["n_clients_per_round_train"] * len(testloaders))
+                                n_clients_per_round_eval =  num_partitions if num_partitions <= 10 else int(fl_config["n_clients_per_round_eval"] * len(testloaders))
+                                fl_seed = fl_config["seed"]
+                                metrics_train_list, metrics_eval_list, metrics_aggregated_train_list, metrics_aggregated_eval_list, test_res = run_fl_experiment(n_comunication_rounds, n_clients_per_round_train, n_clients_per_round_eval, trainloaders, testloaders, centralized_dataloader, net, seed=fl_seed)
+                            except ValueError as e:
+                                print(f"Failed to load partitions: {e}")
+                                metrics_train_list, metrics_eval_list, metrics_aggregated_train_list, metrics_aggregated_eval_list, test_res = np.nan, np.nan, np.nan, np.nan, np.nan
+                            finally:
+                                metrics_to_save = [metrics_train_list, metrics_eval_list, metrics_aggregated_train_list, metrics_aggregated_eval_list, test_res]
+                                metrics_names = ["metrics_train_list", "metrics_eval_list", "metrics_aggregated_train_list", "metrics_aggregated_eval_list", "test_res"]
+                                for metrics_name, metric_to_save in zip(metrics_names, metrics_to_save):
+                                    save_results_dir_path = (f"results-2024-03-09/{single_fds['dataset']}/"
+                                                                            f"{partitioner_signature.__name__}/{metrics_name}.csv")
+                                    
+                                    save_results_dir_path = Path(save_results_dir_path)
+                                    include_header = True
+                                    if save_results_dir_path.exists():
+                                        include_header = False
+                                    save_results_dir_path.parent.mkdir(parents=True, exist_ok=True)
+
+                                    if metrics_name == "test_res":
+                                        pd.DataFrame([[*single_partitioner_config.values(), single_fds.get("seed", "default"), *list(metric_to_save.values())]], columns=[*list(single_partitioner_config.keys()), "fds_seed", *list(metric_to_save.keys())]).to_csv(save_results_dir_path, index=False, mode="a", header=include_header)
+                                    else:
+                                        pd.DataFrame([[*single_partitioner_config.values(), single_fds.get("seed", "default"), metric_to_save]], columns=[*list(single_partitioner_config.keys()), "fds_seed", metrics_name]).to_csv(save_results_dir_path, index=False, mode="a", header=include_header)
+                                    print(f"FL {metrics_name} saved")
+
+                                    
+                                # pd.DataFrame([[*single_partitioner_config.values(), single_fds.get("seed", "default"), metrics_eval_list]], columns=[*list(single_partitioner_config.keys()), "fds_seed", "metrics_train_list"]).to_csv(save_results_dir_path, index=False, mode="a", header=include_header)
+                                # print("FL Training history saved")
+
+
+                                # pd.DataFrame([[dataset_name, metrics_eval_list]], columns=["dataset_name", "metrics_eval_list"]).to_csv("./fl_eval_metrics.csv")
+                                # print("FL Evaluation history saved")
+
+                                # print("FL Training history aggregated:")
+                                # print(metrics_aggregated_train_list)
+                                # pd.DataFrame([[dataset_name, metrics_aggregated_train_list]], columns=["dataset_name", "metrics_aggregated_train_list"]).to_csv("./fl_train_aggregated_metrics.csv")
+
+                                # print("FL Evaluation history aggregated:")
+                                # print(metrics_aggregated_eval_list)
+                                # pd.DataFrame([[dataset_name, metrics_aggregated_eval_list]], columns=["dataset_name", "metrics_aggregated_eval_list"]).to_csv("./fl_eval_aggregated_metrics.csv")
+
+                                # pd.DataFrame([[*single_partitioner_config.values(), single_fds.get("seed", "default"), metrics_fnc.__name__, metric_avg]], columns=[*list(single_partitioner_config.keys()), "fds_seed", "metric_name", "metric_value"]).to_csv(save_results_dir_path, index=False, mode="a", header=include_header)
+
+
+                    
+
+                        # if metrics_fnc.__name__ not in metrics_avg_results:
+                        #     metrics_avg_results[metrics_fnc.__name__] = pd.DataFrame([], columns=[*list(single_partitioner_config.keys()), "fds_seed", "metric_name", "metric_value"])
+                        #     # metrics_avg_results[metrics_fnc.__name__]["fds_seed"] = single_fds.get("seed", "default")
                             # metrics_avg_results[metrics_fnc.__name__]["metric_name"] = metrics_fnc.__name__
                             # metrics_avg_results[metrics_fnc.__name__][
                             #     "metric_value"] = metric_avg
 
                         # metrics_avg_results[metrics_fnc.__name__][
                         #     str(tuple(single_partitioner_config.items()))] = metric_avg
-                        metrics_avg_results[metrics_fnc.__name__].loc[len(metrics_avg_results[metrics_fnc.__name__])] =  [*single_partitioner_config.values(), single_fds.get("seed", "default"), metrics_fnc.__name__, metric_avg]
+                        # metrics_avg_results[metrics_fnc.__name__].loc[len(metrics_avg_results[metrics_fnc.__name__])] =  [*single_partitioner_config.values(), single_fds.get("seed", "default"), metrics_fnc.__name__, metric_avg]
 
                 # Old way of saving the results (not easy for aggregation)
                 # save_results_dir_path = (f"../results/{single_fds['dataset']}/"
@@ -121,19 +193,18 @@ def run_experiments_from_configs(datasets_param_grid, partitioner_param_grid, me
                 # print(f"Metrics saved in {save_results_dir_path}")
                 
                 # Save the heterogeneity metrics results (from a single partitioner [different configurations])
-                for name, value in metrics_avg_results.items():
-                    save_results_dir_path = (f"../results-new/{single_fds['dataset']}/"
-                                                                      f"{partitioner_signature.__name__}/{name}.csv")
-                    save_results_dir_path = Path(save_results_dir_path)
-                    include_header = True
-                    if save_results_dir_path.exists():
-                        include_header = False
-                    save_results_dir_path.parent.mkdir(parents=True, exist_ok=True)
-                    value.to_csv(save_results_dir_path, index=False, mode="a", header=include_header)
+                # for name, value in metrics_avg_results.items():
+                #     save_results_dir_path = (f"../results-new/{single_fds['dataset']}/"
+                #                                                       f"{partitioner_signature.__name__}/{name}.csv")
+                #     save_results_dir_path = Path(save_results_dir_path)
+                #     include_header = True
+                #     if save_results_dir_path.exists():
+                #         include_header = False
+                #     save_results_dir_path.parent.mkdir(parents=True, exist_ok=True)
+                #     value.to_csv(save_results_dir_path, index=False, mode="a", header=include_header)
 
-                if run_fl:
-                    pass
-                    # print(f"Running FL for {single_fds['dataset']} with {partitioner_signature.__name__}")
+                
+                
 
 
 
@@ -150,7 +221,9 @@ if __name__ == "__main__":
         dataset_param_grid = no_natural_datasets_param_grid
         partitioner_param_grid = no_natural_partitioner_configs
     elif MODE == "CUSTOM":
-        dataset_param_grid = [config_femnist]
+        dataset_param_grid = [config_cifar10]
+        # Single seed
+        dataset_param_grid[0]["seed"] = [42]
         partitioner_param_grid = [config_class_constrained]
     else:
         raise ValueError("incorrect mode name")
@@ -159,6 +232,8 @@ if __name__ == "__main__":
         datasets_param_grid=dataset_param_grid,
         partitioner_param_grid=partitioner_param_grid,
         metrics_configs=metrics_configs,
-        run_fl=True
+        run_heterogeneity=False,
+        run_fl=True,
+        fl_configs=fl_configs,
     )
 
