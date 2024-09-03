ceaaf436ab8a89d116c3185fa43290d98a758933
Adam Narozniak <narozniak.adam4@gmail.com>
2024-09-03 21:13:02 +0200

    Update FDS configs

--- configs/fds_configs.py
+++ configs/fds_configs.py
@@ -1,68 +1,84 @@
-config_femnist = {
-    "dataset": ["adamnarozniak/femnist"],
-    "split": ["train"],
-    # "shuffle": [False],
-    "partition_by": ["character"],
-    "label_name": ["character"],
-    "shuffle": [True],
-    "seed": list(range(42, 47))
-
-}
-natural_datasets_param_grid = [
-    # {
-    #     "dataset": ["speech_commands"],
-    #     "subset": ["v0.01"],
-    #     "split": ["train"],
-    #     "shuffle": [False],
-    #     "partition_by": ["speaker_id"],
-    #     "label_name": ["label"]
-    #
-    # },
-    config_femnist,
-    {
-        "dataset": ["flwrlabs/shakespeare"],
-        "split": ["train"],
-        "shuffle": [False],
-        "partition_by": ["character_id"],
-        "label_name": ["y"]
-
-    },
-    {
-        "dataset": ["sentiment140"],
-        "split": ["train"],
-        "shuffle": [False],
-        "partition_by": ["user"],
-        "label_name": ["sentiment"]
-
-    },
-]
-config_cifar10 = {
-    "dataset": ["cifar10"],
-    "label_name": ["label"],
-    "partition_by": ["label"],
-    "split": ["train"],
-    "shuffle": [True],
-    "seed": list(range(42, 47))
-}
 config_mnist = {
     "dataset": ["mnist"],
     "label_name": ["label"],
+    "features_name": ["img"],
     "partition_by": ["label"],
     "split": ["train"],
     "shuffle": [True],
     "seed": list(range(42, 47))
 }
+
+config_cifar10 = {
+    "dataset": ["cifar10"],
+    "label_name": ["label"],
+    "features_name": ["image"],
+    "partition_by": ["label"],
+    "split": ["train"],
+    "shuffle": [True],
+    "seed": list(range(42, 47))
+}
+
 config_cifar100 = {
     "dataset": ["cifar100"],
     "label_name": ["fine_label"],
+    "features_name": ["image"],
     "partition_by": ["fine_label"],
     "split": ["train"],
     "shuffle": [True],
     "seed": list(range(42, 44))
 }
+
+config_femnist = {
+    "dataset": ["flwrlabs/femnist"],
+    "split": ["train"],
+    "partition_by": ["writer_id"],
+    "label_name": ["character"],
+    "features_name": ["image"],
+    "shuffle": [True],
+    "seed": list(range(42, 47))
+
+}
+
+config_speech_commands = {
+        "dataset": ["speech_commands"],
+        "subset": ["v0.01"],
+        "split": ["train"],
+        "shuffle": [False],
+        "partition_by": ["speaker_id"],
+        "label_name": ["label"]   
+}
+
+config_shakespeare = {
+        "dataset": ["flwrlabs/shakespeare"],
+        "split": ["train"],
+        "shuffle": [False],
+        "partition_by": ["character_id"],
+        "label_name": ["y"],
+        "features_name": ["x"],
+
+}
+
+config_sentiment140 = {
+        "dataset": ["sentiment140"],
+        "split": ["train"],
+        "shuffle": [False],
+        "partition_by": ["user"],
+        "label_name": ["sentiment"],
+        "features_name": ["text"],
+
+}
+
+natural_datasets_param_grid = [
+    config_femnist,
+    #config_speech_commands,
+    #config_shakespeare,
+    #config_sentiment140
+]
+
 no_natural_datasets_param_grid = [
     config_cifar10, config_mnist, config_cifar100
 ]
+
 # Add the natural_id datasets to the simulate the division on them
 for natural_dataset_param_grid in natural_datasets_param_grid:
     fds_copy = natural_dataset_param_grid.copy()
@@ -71,4 +87,5 @@ for natural_dataset_param_grid in natural_datasets_param_grid:
     no_natural_datasets_param_grid.append(
         {**fds_copy, "seed": list(range(42, 47))})
 
-fds_param_grids = [*no_natural_datasets_param_grid, *natural_datasets_param_grid]
+all_fds_configs = [*no_natural_datasets_param_grid, *natural_datasets_param_grid]
+fds_used_configs = [config_cifar10, config_cifar100, config_femnist, config_mnist]
