46c03e0890c944be9f4c48abeb450d48499b7e3a
Adam Narozniak <narozniak.adam4@gmail.com>
2024-09-05 00:32:24 +0200

    Fix CNN shapes for MNIST

--- heterogeneity/fl/model.py
+++ heterogeneity/fl/model.py
@@ -35,19 +35,20 @@ class CNNNet(nn.Module):
         return self.fc3(x)
     
 class CNNNetGray(nn.Module):
+    # For MNIST 28x28 images
     def __init__(self, num_classes) -> None:
         super(CNNNetGray, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        x = x.view(-1, 16 * 4 * 4)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
