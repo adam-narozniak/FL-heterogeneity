921a41afa60459824ad3618463f1e8a2f0d1924a
Adam Narozniak <narozniak.adam4@gmail.com>
2024-09-04 14:30:58 +0200

    Extend FL config by early_stopping

--- configs/fl_configs.py
+++ configs/fl_configs.py
@@ -1,3 +1,9 @@
 fl_configs = [
-    {"n_comunication_rounds": 1_000, "n_clients_per_round_train": 0.1, "n_clients_per_round_eval": 0.1, "seed": 42},  
+    {
+        "n_comunication_rounds": 50,
+        "n_clients_per_round_train": 0.1,
+        "n_clients_per_round_eval": 0.1,
+        "seed": 42,
+        "early_stopping": False,
+    },
 ]
