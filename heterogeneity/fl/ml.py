6a1de6bc95ce3329c56eb5c854afe51a8f729924
Adam Narozniak <narozniak.adam4@gmail.com>
2024-09-05 14:36:11 +0200

    Move train test to ml

---
+++ heterogeneity/fl/ml.py
@@ -0,0 +1,59 @@
+import ray
+import torch
+
+from heterogeneity.fl.utils import DEVICE, get_weights
+
+
+@ray.remote(num_cpus=1)
+def train(net, trainloader, epochs, features_name="img", label_name="label"):
+    """Train the model on the training set."""
+    net.to(DEVICE)
+    criterion = torch.nn.CrossEntropyLoss()
+    # optimizer = torch.optim.SGD(net.parameters(), lr=0.01, momentum=0.9, weight_decay=0.00001)
+    optimizer = torch.optim.Adam(net.parameters())
+    net.train()
+    train_loss, train_acc = 0.0, 0.0
+    num_examples = len(trainloader.dataset)
+    for _ in range(epochs):
+        epoch_correct, epoch_loss = 0, 0.0
+        for batch in trainloader:
+            images = batch[features_name].to(DEVICE)
+            labels = batch[label_name].to(DEVICE)
+            optimizer.zero_grad()
+            outputs = net(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+            epoch_correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+        epoch_loss /= num_examples
+        epoch_acc = epoch_correct / num_examples
+        train_loss += epoch_loss
+        train_acc += epoch_acc
+    train_loss /= epochs
+    train_acc /= epochs
+    return (
+        get_weights(net),
+        len(trainloader.dataset),
+        {"train_loss": train_loss, "train_acc": train_acc},
+    )
+
+
+@ray.remote(num_cpus=1)
+def test(net, testloader, features_name="img", label_name="label"):
+    """Test the model on the test set."""
+    net.to(DEVICE)
+    criterion = torch.nn.CrossEntropyLoss(reduction="sum")
+    correct, total_loss = 0, 0.0
+    net.eval()
+    num_examples = len(testloader.dataset)
+    with torch.no_grad():
+        for batch in testloader:
+            images = batch[features_name].to(DEVICE)
+            labels = batch[label_name].to(DEVICE)
+            outputs = net(images)
+            total_loss += criterion(outputs, labels).item()
+            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+    accuracy = correct / num_examples
+    avg_loss = total_loss / num_examples
+    return len(testloader.dataset), {"eval_loss": avg_loss, "eval_acc": accuracy}
--- heterogeneity/fl/utils.py
+++ heterogeneity/fl/utils.py
@@ -4,7 +4,6 @@ import warnings
 from collections import OrderedDict
 from typing import List, Dict, Union
 
-import ray
 import torch
 
 from tqdm import tqdm
@@ -22,56 +21,6 @@ def set_weights(net, parameters):
     params_dict = zip(net.state_dict().keys(), parameters)
     state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
     net.load_state_dict(state_dict, strict=True)
-
-
-@ray.remote(num_cpus=1) # I used to have memory=100 * 10 ** 6 but it seems to be incorrectly estimated by me. I don't know why, I tried to run this script but it seems to be incorrect
-def train(net, trainloader, epochs, features_name="img", label_name="label"):
-    """Train the model on the training set."""
-    net.to(DEVICE)
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(net.parameters(), lr=0.01, momentum=0.9, weight_decay=0.00001)#torch.optim.Adam(net.parameters())
-    net.train() 
-    train_loss, train_acc = 0.0, 0.0
-    num_examples = len(trainloader.dataset)
-    for _ in range(epochs):
-        epoch_correct, epoch_loss = 0,  0.0
-        for batch in trainloader:
-            images = batch[features_name].to(DEVICE)
-            labels = batch[label_name].to(DEVICE)
-            optimizer.zero_grad()
-            outputs = net(images)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            epoch_loss += loss.item()
-            epoch_correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
-        epoch_loss /= num_examples
-        epoch_acc = epoch_correct / num_examples
-        train_loss += epoch_loss
-        train_acc += epoch_acc
-    train_loss /= epochs
-    train_acc /= epochs
-    return get_weights(net), len(trainloader.dataset), {"train_loss": train_loss, "train_acc": train_acc}
-
-@ray.remote(num_cpus=1)
-def test(net, testloader, features_name="img", label_name="label"):
-    """Test the model on the test set."""
-    net.to(DEVICE)
-    criterion = torch.nn.CrossEntropyLoss(reduction="sum")
-    correct, total_loss = 0, 0.0
-    net.eval()
-    num_examples = len(testloader.dataset)
-    with torch.no_grad():
-        for batch in testloader:
-            images = batch[features_name].to(DEVICE)
-            labels = batch[label_name].to(DEVICE)
-            outputs = net(images)
-            total_loss += criterion(outputs, labels).item()
-            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
-    accuracy = correct / num_examples
-    avg_loss = total_loss / num_examples
-    return len(testloader.dataset), {"eval_loss": avg_loss, "eval_acc": accuracy}
-
 
 
 def weighted_average(metrics_list: List[Dict[str, Union[int, float]]], 
