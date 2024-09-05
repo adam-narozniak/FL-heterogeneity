49b5efebad834fedbed41d7b844cfefc5b8723be
Adam Narozniak <narozniak.adam4@gmail.com>
2024-09-05 14:47:04 +0200

    Add return hint types to ml_utils

--- heterogeneity/fl/ml_utils.py
+++ heterogeneity/fl/ml_utils.py
@@ -1,6 +1,8 @@
 import warnings
 from collections import OrderedDict
+from typing import List
 
+import numpy as np
 import torch
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
@@ -8,11 +10,11 @@ warnings.simplefilter(action="ignore", category=FutureWarning)
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-def get_weights(net):
+def get_weights(net) -> List[np.ndarray]:
     return [val.cpu().numpy() for _, val in net.state_dict().items()]
 
 
-def set_weights(net, parameters):
+def set_weights(net, parameters) -> None:
     params_dict = zip(net.state_dict().keys(), parameters)
     state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
     net.load_state_dict(state_dict, strict=True)
