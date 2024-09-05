1771dae21bcf2f1672f855dae0efcef2b664d7b1
Adam Narozniak <narozniak.adam4@gmail.com>
2024-09-05 14:41:14 +0200

    Reformat ml_utils

--- heterogeneity/fl/ml_utils.py
+++ heterogeneity/fl/ml_utils.py
@@ -1,17 +1,12 @@
-import argparse
-from typing import Tuple, Union
 import warnings
 from collections import OrderedDict
-from typing import Union
 
 import torch
 
-from tqdm import tqdm
-
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 
 def get_weights(net):
     return [val.cpu().numpy() for _, val in net.state_dict().items()]
@@ -21,5 +16,3 @@ def set_weights(net, parameters):
     params_dict = zip(net.state_dict().keys(), parameters)
     state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
     net.load_state_dict(state_dict, strict=True)
-
-
