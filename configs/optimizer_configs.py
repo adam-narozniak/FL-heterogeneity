da2314c35f51ab4e3e5018dea392d0d2ad1314f4
Adam Narozniak <narozniak.adam4@gmail.com>
2024-09-06 19:19:20 +0200

    Restructure optimizers config

--- configs/optimizer_configs.py
+++ configs/optimizer_configs.py
@@ -1,11 +1,11 @@
 from torch.optim import SGD, Adam
 
-optimizer_configs = [
-    {
-        "object": [SGD],
-        "lr": [0.1, 0.01, 0.001],
-        "momentum": [0.9],
-        "weight_decay": [0.00001, 0.0001],
-    },
-    {"object": [Adam]},
-]
+sgd_config = {
+    "object": [SGD],
+    "lr": [0.1, 0.01, 0.001],
+    "momentum": [0.9],
+    "weight_decay": [0.00001, 0.0001],
+}
+adam_config = {"object": [Adam]}
+
+optimizer_configs = [sgd_config, adam_config]
