d036b0388fbbdf02d928939c6fb17c2ecb6c5905
Adam Narozniak <narozniak.adam4@gmail.com>
2024-09-06 04:25:52 +0200

    Update the optimizer weights grid

--- configs/optimizer_configs.py
+++ configs/optimizer_configs.py
@@ -5,7 +5,7 @@ optimizer_configs = [
         "object": [SGD],
         "lr": [0.1, 0.01, 0.001],
         "momentum": [0.9],
-        "weight_decay": [0.01, 0.001, 0.0001],
+        "weight_decay": [0.00001, 0.0001],
     },
     {"object": [Adam]},
 ]
